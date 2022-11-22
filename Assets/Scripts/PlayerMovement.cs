c25ba5f94257e5cb56cf8a149b685b325011b359
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2022-11-22 08:49:15 +0100

    Further development of player movement controller
    [CHANGED] Player controller
    [ADJUSTED] Physicsmaterial

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private string jump = "space";
     private Vector2 upDirection = new Vector2(0,1);
     private Vector2 forwardDirection;
+    private Vector2 velocity;
     private Rigidbody2D rigidBody;
-    private Collider2D collider;
     [SerializeField] private float Gravity = 987;
     [SerializeField] private float WalkSpeed;
     [SerializeField] private float drag;
@@ -21,55 +21,51 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
-        collider = this.gameObject.GetComponent<Collider2D>();
 
         forwardDirection = upDirection.Rotate(-90f);
-
     }
 
     void Update()
     {
+        velocity = new Vector2(0, 0);
         if (rigidBody.IsTouchingLayers()) //If on ground
         {
             if (Input.GetKeyDown(jump))
             {
                 //RigidBody.AddForce(upDirection * Gravity * Time.deltaTime);
-                rigidBody.AddForce(upDirection * JumpStrength);
+                velocity += upDirection * JumpStrength;
             }
             else if (!Input.GetKey(forward) && !Input.GetKey(backward) || Input.GetKey(forward) && Input.GetKey(backward))
             {
-                rigidBody.AddForce(forwardDirection * rigidBody.velocity.Project(forwardDirection) * -drag * Time.deltaTime); //Friction
+                velocity += forwardDirection * velocity.Project(forwardDirection) * -drag * Time.deltaTime; //Friction
             }
             if (Input.GetKey(forward))
             {
-                rigidBody.AddForce(forwardDirection * Mathf.Clamp((rigidBody.velocity.Project(forwardDirection)), -1f, 0f) * -drag * Time.deltaTime); //Apply friction IF going left while pressing right
-                rigidBody.AddForce(forwardDirection * WalkSpeed * Time.deltaTime);
+                velocity += forwardDirection * WalkSpeed * Time.deltaTime;
             }
             if (Input.GetKey(backward))
             {
-                rigidBody.AddForce(forwardDirection * Mathf.Clamp((rigidBody.velocity.Project(forwardDirection)), 0f, 1f) * -drag * Time.deltaTime); //Apply friction IF going right while pressing left
-                rigidBody.AddForce(-forwardDirection * WalkSpeed * Time.deltaTime);
+                velocity -= forwardDirection * WalkSpeed * Time.deltaTime;
             }
         }
         else //If in air
         {
-            rigidBody.AddForce(upDirection * -Gravity * Time.deltaTime);
+            velocity += upDirection * -Gravity * Time.deltaTime; //Gravity
 
             if (!Input.GetKey(forward) && !Input.GetKey(backward) || Input.GetKey(forward) && Input.GetKey(backward))
             {
-                rigidBody.AddForce(forwardDirection * rigidBody.velocity.Project(forwardDirection) * -airDrag * Time.deltaTime);
+                velocity += forwardDirection * velocity.Project(forwardDirection) * -airDrag * Time.deltaTime; //Drag
             }
             if (Input.GetKey(forward))
             {
-                rigidBody.AddForce(forwardDirection * Mathf.Clamp((rigidBody.velocity.Project(forwardDirection)), -1f, 0f) * -airDrag * Time.deltaTime); //Apply friction IF going left while pressing right
-                rigidBody.AddForce(forwardDirection * WalkSpeed * Time.deltaTime);
+                velocity += forwardDirection * WalkSpeed * Time.deltaTime;
             }
             if (Input.GetKey(backward))
             {
-                rigidBody.AddForce(forwardDirection * Mathf.Clamp((rigidBody.velocity.Project(forwardDirection)), 0f, 1f) * -airDrag * Time.deltaTime); //Apply friction IF going right while pressing left
-                rigidBody.AddForce(-forwardDirection * WalkSpeed * Time.deltaTime);
+                velocity -= forwardDirection * WalkSpeed * Time.deltaTime;
             }
         }
-        rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxVelocity);
+        //velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
+        rigidBody.AddForce(velocity);
     }
 }
